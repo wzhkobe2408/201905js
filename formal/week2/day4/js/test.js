03cea438796bc4cc380528877c407438062563df
mabin <1164664451@qq.com>
2019-06-07 11:06:44 +0800

    day4上午

---
+++ formal/week2/day4/js/test.js
@@ -0,0 +1,4 @@
+var ary100 = [100, 200, 300, 400];
+function getName() {
+  console.log('MY_name—— MABIN');
+}
--- formal/week2/day4/js/utils.js
+++ formal/week2/day4/js/utils.js
@@ -1,2 +1,29 @@
-// 封装一个工具集
+// 封装一个工具集：增强代码的可复用性，提升开发效率；
+// utils 工具包，这里面提供了常用的方法；
+
+window.utils = (function () {
+  /**
+   * @desc 类数组转数组
+   * @param arrLike 类数组对象
+   * @returns {Array} 类数组对象转成的数组
+   */
+  function arrLikeToAry(arrLike) {
+    try {
+      return Array.from(arrLike);
+    } catch (e) {
+      var ary = []; //
+      for (var i = 0; i < arrLike.length; i++) {
+        ary.push(arrLike[i]);
+      }
+      return ary;
+    }
+  }
+  function toJson() {
+
+  }
+  return {
+    arrLikeToAry: arrLikeToAry
+    // arrLikeToAry
+  }
+})();
 

0ef605da51d41e8b297e3c697c83bbea233ecb9d
mabin <1164664451@qq.com>
2019-06-12 13:12:55 +0800

    week3-day2

--- formal/week3/day3/js/2-正则高级应用.js
+++ formal/week3/day3/js/2-正则高级应用.js
@@ -1,105 +1,65 @@
+/*
+* 高级应用：
+*
+* */
 
-
-// 1. 配合replace使用
-// String.prototype.replace(匹配内容, '替换内容');
-
-let str = 'zhufeng2018 zhufeng2019';
-
-// 把字符串中的 zhufeng 替换成 珠峰
-
-let result = str.replace('zhufeng', '珠峰');
-console.log(result); // 珠峰2018 zhufeng2019
-
-// 一次只能替换一个，那么有没有什么办法一次都替换掉呢？
-
-// replace(正则, callback); replace配合正则使用时
+// 1. 正则结合replace方法（replace是字符串的方法）；
+let str = 'zhufeng2018 zhufeng2019 zhufeng2020 zhufeng2021';
+// 需求：把zhufeng拼音换成汉字：
+let r = str.replace('zhufeng', '珠峰').replace('zhufeng', '珠峰').replace('zhufeng', '珠峰');
+// console.log(r);
+// ? 有没有办法一次性都替换掉？
+// 正则+replace
+let reg = /(zhu)(feng)/g;
 let i = 0;
-let reg = /(zhufeng)/g;
 let r2 = str.replace(reg, function (...arg) {
-	// 回调函数的参数：
-	// 第一个：整个正则匹配到的内容
-	// 如果有分组，会从第二个参数
-	// 倒数第二个参数是 本次捕获的的起始索引位置
-	// 最后一个参数是当前原始字符串
-	i++;
-	console.log(arg);
-	return '珠峰'
+  // 正则能够在字符串中匹配到多少个，这个回调函数就会执行多少回；
+  // 用回调函数的返回值去替换捕获到的内容
+
+  // 在替换的过程中，回调函数会接收到几个参数：
+  // 第一个：大正则捕获到的内容
+  // 如果有分组从第二个参数开始就是分组捕获到的内容
+  // 倒数第二个是捕获到的索引位置
+  // 倒数第一个是原始字符串
+  i++;
+  // console.log(arg);
+  return 'ZF' + i;
 });
-console.log(r2);
-console.log(i); // 2 说明回调函数执行了2次；
+// console.log(i);
+// console.log(r2);
 
-// 2. 格式化时间字符串: 按照时间模板格式时间字符串
+// 2. 格式化时间字符串：根据给定的时间模板，把时间字符串格式化
 
-// "2019/6/30 17:50:23" => '06-30 17:50'
+// 时间字符串： "2019/6/12 12:38:23"
+// 模板：'{0}年{1}月{2}日 {3}时{4}分{5}秒' -> "2019年06月12号 12点38分23秒"
+// 模板：'{0}-{1}-{2} {3}:{4}:{5}' -> "2019-06-12 12:38:23"
+// 2.1 用正则把时间字符串中的数字都取出来
+let timeStr = "2019/6/12 12:38:23";
+let tmpStr = '{0}年{1}月{2}日 {3}时{4}分{5}秒';
+let reg3 = /\d+/g;
+let timeAry = timeStr.match(reg3);
 
-// 2.1 获取时间字符串中所有时间数字
-let str2 = "2019/6/30 17:50:23";
-let reg2 = /\d+/g;
-let ary = str2.match(reg2);
-console.log(ary);
-// 2.2 判断如果这些数字比10小，需要补0
-let addZero = ary.map(item => +item < 10 ? '0' + item : item);
-console.log(addZero);
+// 2.2 把小于10的数字补0
+let timeAry2 = timeAry.map(item => item < 10 ? '0' + item : item);
+// console.log(timeAry2); // ["2019", '06', '12', '12', '38', '23']
 
-// 2.3 根据模板把时间格式化
-let tmp = '{0}年{1}月{2}日 {3}时{4}分{5}秒';
-let tmpReg = /\{(\d)\}/g;
-let result2 = tmp.replace(tmpReg, function ([, index]) {
-	return ary[index];
+// 2.3 利用正则把模板中占位符替换成时间数字
+let reg4 = /\{(\d)\}/g; // 我们要使用{}的原义，所以需要转义，因为花括号是用来写量词的；
+let timeResult = tmpStr.replace(reg4, function (a, b) {
+  // 这个回调函数的参数：第一个时大正则捕获到的内容，第二个是正则第一个分组捕获的内容
+  return timeAry2[b];
 });
-console.log(result2);
 
-// 扩展到原型上
-String.prototype.myFormatTime = function (tmp = '{0}年{1}月{2}日 {3}时{4}分{5}秒') {
-	let ary = this.match(/\d+/g).map(item => +item < 10 ? '0' + item : item);
-	return tmp.replace(/\{(\d)\}/g, (...[, index]) => ary[index]);
+// console.log(timeResult);
+String.prototype.formatTime = function (tmp = '{0}年{1}月{2}日 {3}时{4}分{5}秒') {
+  let reg = /\d+/g;
+  // 第一步：把时间数字捕获到
+  let timeAry = this.match(reg).map(
+    item => item < 10 ? '0' + item : item
+  );
+  let reg2 = /\{(\d)\}/g;
+  return tmp.replace(reg2, (big, index) => timeAry[index]);
 };
 
-console.log("2018/4/30 17:50:23".myFormatTime('{1}-{2} {3}:{4}:{5}'));
-
-
-// 3. url查询参数格式化
-let url = 'http://www.zhufeng.cn?name=mabin&age=18&form=zf&addre=hebei';
-
-let params = {};
-let reg3 = /([^?=&]+)=([^?=&]+)/g;
-url.replace(reg3, (str, key, value) => params[key] = value);
-console.log(params);
-
-String.prototype.urlSeries = function () {
-	let params = {};
-	let reg3 = /([^?=&]+)=([^?=&]+)/g;
-	this.replace(reg3, (str, key, value) => params[key] = value);
-	return params
-};
-let result3 = url.urlSeries();
-console.log(result3);
-
-
-// 4. 给数字千分符
-
-// 千分符
-// '2121345465.12' => 2,121,345,465.12
-
-let str4 = '2121345465';
-let reg4 = /\d(?=(\d{3})+(\.\d+)?$)/g;
-let r4 = str4.replace(reg4, function (a) {
-	return a + ','
-});
-console.log(r4);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("2019/6/12 12:38:23".formatTime('{1}-{2} {3}:{4}:{5}'));
 

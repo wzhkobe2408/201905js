164265c210c24e227dcd4e5d1fa5e4d715f91ca5
mabin <1164664451@qq.com>
2019-07-08 18:41:22 +0800

    week7-day1

---
+++ formal/week7/day1/js/5-uri、url、urn.js
@@ -0,0 +1,20 @@
+
+// URI / URN / URL
+// URI: 统一资源标识符；
+// URN: 统一资源名称；
+// URL: 统一资源定位符
+
+// 重点讲URL:
+// 常见的URL
+// https://www.dahai.com:443/courseMode.html?id=1&key=value&name=mb#top
+
+// http/https:// 协议
+// www.dahai.com 域名
+// :443 端口号
+// /courseMode.html 路径
+// ?id=1&key=value&name=mb 问号传参（可以有可以没有的），是客户端传递给服务端的数据；
+// #top 锚点（哈希）
+
+
+// 只请求一个 www.baidu.com 域名时，相当于请求 www.baidu.com/index.html
+// www.baidu.com 对应的是服务器上的一个文件夹，访问www.baidu.com的时候，服务器会默认把这个文件夹下的html文件给客户端；

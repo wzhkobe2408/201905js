c14a3539bd76c69f24884bdd3322c05141225036
mabin <1164664451@qq.com>
2019-08-19 13:01:20 +0800

    week12-day1上午 todo over

--- formal/week12/day1/src/components/TodoFooter.js
+++ formal/week12/day1/src/components/TodoFooter.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 
-export default class TodoFooter extends Component {
+import { connect } from 'react-redux'
+
+import actions from '../store/action/todo'
+
+class TodoFooter extends Component {
   render () {
-    return (<div>
-      TodoFooter
-    </div>)
+    let { filter, changeCurrentFilter } = this.props
+    return (<nav className="nav nav-pills" onClick={(e) => {
+      // 点击 a 标签的时候，修改 store 中的 filter 属性为对应的值
+      // let filter = e.target.getAttribute('data-filter')
+      // console.log(e.target.dataset)
+       // dataset 是 h5 新增的获取元素行内以 data- 开头的行内属性的属性值；dataset 是一个对象，属性名是不含 data- 的属性名；
+      let filter = e.target.dataset.filter
+      changeCurrentFilter(filter)
+    }}>
+      <li className={filter === 'all' ? 'active' : ''}>
+        <a data-filter="all" data-x="x" data-y="y">全部</a>
+      </li>
+      <li className={filter === 'unfinished' ? 'active' : ''}>
+        <a data-filter="unfinished">未完成</a>
+      </li>
+      <li className={filter === 'finished' ? 'active' : ''}>
+        <a data-filter="finished">完成</a>
+      </li>
+    </nav>)
   }
 }
+
+export default connect(state => ({...state}), actions)(TodoFooter)
--- formal/week12/day1/src/components/TodoList.js
+++ formal/week12/day1/src/components/TodoList.js
@@ -7,10 +7,24 @@ import actions from '../store/action/todo' // actionCreator 对象
 import ListItem from './ListItem'
 
 class TodoList extends Component {
+  filterTodo = () => {
+    // 为了实现筛选的效果，我们的列表不能再直接渲染全量的数据；
+    // 列表应该渲染根据 store 中的 filter 过滤出满足筛选条件数据；
+    let { filter, list } = this.props
+    switch (filter) {
+      case 'all':
+        return list
+      case 'unfinished':
+        return list.filter(item => !item.isSelected)
+      case 'finished':
+        return list.filter(item => item.isSelected)
+    }
+  }
   render () {
+    let filterList = this.filterTodo()
     return (<ul className="list-group">
       {
-        this.props.list.map((item, index) => {
+        filterList.map((item, index) => {
           return <ListItem
             key={item.id}
             changeSelected={this.props.changeSelected}

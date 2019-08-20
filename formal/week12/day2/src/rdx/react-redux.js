9b030beb2b286831657d46dd7daaaa74f0944b8a
mabin <1164664451@qq.com>
2019-08-20 18:19:40 +0800

    week12-day2 下午 react-redux

--- formal/week12/day2/src/rdx/react-redux.js
+++ formal/week12/day2/src/rdx/react-redux.js
@@ -18,7 +18,23 @@ let connect1 = function (mapStateToProps, mapDispatchToProps) {
   }
 }
 
-let bind
+// 这个方法可允许 mapDispatchToProps 传递一个 actionCreator 对象
+let bindActionCreators = (actions, dispatch) => {
+  let obj = {}
+  for (let key in actions) {
+    obj[key] = (...arg) => dispatch(actions[key](...arg))
+  }
+ /* let o = {
+    add (...arg) {
+      dispatch(actions.add(...arg))
+    },
+    minus (...arg) {
+      dispatch(actions.minus(...arg))
+    }
+  }*/
+  console.log(obj)
+  return obj
+}
 
 let connect = (mapStateToProps, mapDispatchToProps) => (Component) => {
 
@@ -41,7 +57,14 @@ let connect = (mapStateToProps, mapDispatchToProps) => (Component) => {
 
     render () {
       // mapDispatchToProps 的返回值是变更状态的对象，我们把这个对象变成 Component 的 props
-      let dispatchToProps = mapDispatchToProps(this.context.dispatch)
+      let dispatchToProps = {};
+      // mapDispatchToProps 可以简写成一个 actionCreator 对象，所以这里我们需要处理一下；如果是一个对象，我们需要调用 bindActionCreators 来处理
+      if (typeof mapDispatchToProps === 'object') {
+        dispatchToProps = bindActionCreators(mapDispatchToProps, this.context.dispatch)
+      } else {
+        dispatchToProps = mapDispatchToProps(this.context.dispatch)
+      }
+
       return <Component {...this.state}  {...dispatchToProps} />
     }
   }
